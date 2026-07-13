7086a1c1084b57d69ee475f5f47b3b934321d388
Mateusz Czuper <mateusz300102@gmail.com>
2026-07-13 16:37:55 +0200

    feat: add Patient entity

---
+++ src/main/java/com/Czupson/medical_clinic/model/Patient.java
@@ -0,0 +1,29 @@
+package com.Czupson.medical_clinic.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String password;
+    private String idCardNo;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private LocalDate birthday;
+}
